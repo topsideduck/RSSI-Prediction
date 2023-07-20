1129443e0a68afab275675ea1db87aa74d75b946
Harikeshav R <r.harikeshav@gmail.com>
2023-07-20 22:55:06 +0530

    Remove redundant code

--- RSSI/models/rectangle/linear_regression.py
+++ RSSI/models/rectangle/linear_regression.py
@@ -21,22 +21,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Create a grid of coordinates covering the entire room
-x_max, y_max = 6, 4
-resolution = 0.1  # Smaller resolution for more accurate predictions
-xx, yy = np.meshgrid(
-    np.arange(0, x_max + resolution, resolution),
-    np.arange(0, y_max + resolution, resolution),
-)
-
-# Reshape the grid into a flat array for prediction
-grid_points = np.column_stack((xx.ravel(), yy.ravel()))
-
-# Make predictions on the grid
-predicted_rssi_data = model.predict(grid_points).reshape(xx.shape)
-
-# Calculate the R-squared (accuracy) of the model using the test dataset
+# Make predictions on the test set
 y_pred_test = model.predict(X_test)
+
+# Calculate the R-squared (accuracy) of the model
 accuracy = r2_score(y_test, y_pred_test)
 
 print("R-squared (Accuracy):", accuracy)
