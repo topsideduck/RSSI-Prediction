189246b19d6cd8360c4a5024de382fe234235d0c
Harikeshav R <r.harikeshav@gmail.com>
2023-07-20 23:07:27 +0530

    Enhance K nearest neighbors model for RSSI prediction

--- RSSI/models/rectangle/k_nearest_neighbour.py
+++ RSSI/models/rectangle/k_nearest_neighbour.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import MinMaxScaler
 
 # Load the generated dataset
 dataset = pd.read_csv("data/rssi_dataset_rectangle.csv")
@@ -11,14 +12,31 @@ dataset = pd.read_csv("data/rssi_dataset_rectangle.csv")
 X = dataset[["x", "y"]].values
 y = dataset["rssi"].values
 
+# Scale the features to the range [0, 1]
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
 # Split the dataset into training and testing sets (80% training, 20% testing)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Create and train the KNN model
-k_neighbors = 5  # Number of nearest neighbors to consider
-knn_model = KNeighborsRegressor(n_neighbors=k_neighbors)
+# Find the optimal value of k using cross-validation
+best_accuracy = 0.0
+best_k = 1
+
+for k in range(1, 21):
+    knn_model = KNeighborsRegressor(n_neighbors=k)
+    knn_model.fit(X_train, y_train)
+    y_pred = knn_model.predict(X_test)
+    accuracy = r2_score(y_test, y_pred)
+
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        best_k = k
+
+# Train the KNN model using the best value of k
+knn_model = KNeighborsRegressor(n_neighbors=best_k)
 knn_model.fit(X_train, y_train)
 
 # Make predictions on the test set
@@ -27,6 +45,7 @@ y_pred_test = knn_model.predict(X_test)
 # Calculate the R-squared (accuracy) of the model
 accuracy = r2_score(y_test, y_pred_test)
 
+print("Best k:", best_k)
 print("R-squared (Accuracy):", accuracy)
 
 # Create a scatter plot to compare predicted vs. actual RSSI values
