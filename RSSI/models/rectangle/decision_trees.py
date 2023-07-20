ba435d4c80ecf27c069cd6c05989a56ba90a2fd6
Harikeshav R <r.harikeshav@gmail.com>
2023-07-20 23:08:58 +0530

    Maximize decision trees model for RSSI prediction

--- RSSI/models/rectangle/decision_trees.py
+++ RSSI/models/rectangle/decision_trees.py
@@ -16,17 +16,31 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Create and train the Decision Trees model
-max_depth = 5  # Maximum depth of the decision tree
-decision_tree_model = DecisionTreeRegressor(max_depth=max_depth)
-decision_tree_model.fit(X_train, y_train)
+# Find the optimal value for max_depth using cross-validation
+best_accuracy = 0.0
+best_max_depth = 1
+
+for depth in range(1, 21):
+    dt_model = DecisionTreeRegressor(max_depth=depth, random_state=42)
+    dt_model.fit(X_train, y_train)
+    y_pred = dt_model.predict(X_test)
+    accuracy = r2_score(y_test, y_pred)
+
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        best_max_depth = depth
+
+# Train the Decision Trees model using the best value of max_depth
+dt_model = DecisionTreeRegressor(max_depth=best_max_depth, random_state=42)
+dt_model.fit(X_train, y_train)
 
 # Make predictions on the test set
-y_pred_test = decision_tree_model.predict(X_test)
+y_pred_test = dt_model.predict(X_test)
 
 # Calculate the R-squared (accuracy) of the model
 accuracy = r2_score(y_test, y_pred_test)
 
+print("Best max_depth:", best_max_depth)
 print("R-squared (Accuracy):", accuracy)
 
 # Create a scatter plot to compare predicted vs. actual RSSI values
